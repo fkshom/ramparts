f26e4d05e3349e4dda7b3df68d0f584d154b1b96
Shoma FUKUDA <fkshom@gmail.com>
2021-03-19 23:38:15 +0900

    first commit

---
+++ spec/ramparts_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Ramparts do
+  it "has a version number" do
+    expect(Ramparts::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
